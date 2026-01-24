8074505133c1e0c034be1fcf7e2d3fd8ca9a7e91
TedYang <tedyang020105@outlook.com>
2026-01-24 01:22:13 -0800

    add swap and SelectSort again

--- src/main/java/com/zhiyuan/algorithm/sort/SelectSort.java
+++ src/main/java/com/zhiyuan/algorithm/sort/SelectSort.java
@@ -1,2 +1,24 @@
-package com.zhiyuan.algorithm.sort;public class SelectSort {
+package com.zhiyuan.algorithm.sort;
+
+import com.zhiyuan.algorithm.util.ArrayUtils;
+
+public class SelectSort {
+    public static int[] selectSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return arr;
+        }
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            if (minIndex != i) {
+                ArrayUtils.swap(arr, i, minIndex);
+            }
+        }
+        return arr;
+    }
 }
--- src/main/java/com/zhiyuan/algorithm/util/ArrayUtils.java
+++ src/main/java/com/zhiyuan/algorithm/util/ArrayUtils.java
@@ -1,2 +1,12 @@
-package com.zhiyuan.algorithm.util;public class ArrayUtils {
+package com.zhiyuan.algorithm.util;
+
+public class ArrayUtils {
+    public static void swap(int[] arr, int i, int j) {
+        if (arr == null || i < 0 || j < 0 || i >= arr.length || j >= arr.length) {
+            throw new IllegalArgumentException("Invalid indices or array");
+        }
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
 }
