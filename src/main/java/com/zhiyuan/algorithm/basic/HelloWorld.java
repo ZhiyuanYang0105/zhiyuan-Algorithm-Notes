62c5dab406c2c4831bc42de33e9a0e33321d7963
TedYang <tedyang020105@outlook.com>
2026-02-07 14:56:22 -0800

    another test git

--- src/main/java/com/zhiyuan/algorithm/basic/HelloWorld.java
+++ src/main/java/com/zhiyuan/algorithm/basic/HelloWorld.java
@@ -3,6 +3,6 @@ package com.zhiyuan.algorithm.basic;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("你好，世界");
+        System.out.println("你好，世界，我是杨致远");
     }
 }
